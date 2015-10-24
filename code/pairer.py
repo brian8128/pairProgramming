b6f3d16330d3595b90e9bec1ac8a62dc84f43811
Brian Bargh <brian.bargh@gmail.com>
2015-10-24 14:55:18 -0700

    Working?

--- code/pairer.py
+++ code/pairer.py
@@ -1,42 +1,125 @@
+import itertools
+
+
+def f(k, b):
+    """
+    INPUT: integer between 0 and n
+    Canonical translation from k -> (r,s)
+    Finds (r, s) such that s < self.a and r * self.b + s = k
+    """
+    return k / b, k % b
+
+
+def finv(r, s, b):
+    """
+    INPUT: r,s as above
+    OUTPUT: k as above
+    Canonical translation (r,s) -> k
+    """
+    return r * b + s
+
+
+def combine_pairings(pa, pb, b):
+    """
+    INPUT: Two lists of tuples representing pairings on sub problems of size a and b
+    OUTPUT: One list of tuples representing a pairing on a problem of size n = a * b
+    """
+    result = []
+    itr = itertools.product(pa,pb)
+    for pair1, pair2 in itr:
+        comb = (finv(pair1[0], pair2[0], b), finv(pair1[1], pair2[1], b))
+        result.append(comb)
+
+    return result
+
+
+def get_pairing_power_of_2(s, b):
+    """
+    INPUT: s - day number between 0 and b-1, b number of
+    OUTPUT: List of tuples representing the days partnerships
+
+    This function creates a pairing on day s for b students.
+    """
+    if bin(b).count("1") != 1:
+        raise ValueError('B MUST BE A POWER OF 2 READ THE FUNCTION NAME {0} IS UNACCEPTABLE'.format(b))
+
+    result = []
+    for i in range(b):
+        result.append((i, i ^ s))
+    return result
+
+
+def get_pairing_double_odd(r, a):
+    if a % 2 == 1 or a % 4 == 0:
+        raise ValueError("A MUST BE TWICE AN ODD NUMBER READ THE FUNCTION NAME NOT {0}".format(a))
+    if r < 0 or r >= a:
+        raise ValueError("R MUST BE BETWEEN 0 AND {1}-1 NOT {0}".format(r, a))
+
+    # There are a-1 pairings.  For each r from 1 to a-1 we should get a different pairing.
+
+    # For each odd q such that 1 <= q < a, q != a/2, we get a pairing by pairing every even i with i + q % a
+    # This is a/2-1 pairings.  These will be numbered 1 to a/2-1.
+
+    # For each q from 0 to a/2-1 we get a pairing by matching q with q + a/2 and q-i with q+i for i in range(1, a/2-1)
+    # This is a/2 pairings.  These will be numbered a/2 to a - 1
+
+    # pairing number 0 is the identity (non)pairing.
+
+    result = []
+    if r == 0:
+        for i in range(a):
+            result.append((i, i))
+        return result
+    elif r < a/2:
+        q = 2 * r - 1
+        # skip the case where q = a/2
+        if q >= a/2:
+            q += 2
+        for i in range(0, a-1, 2):
+            result.append((i, (i+q)%a))
+        return result
+    else:
+        assert(r < a)
+        q = r - a/2
+        result.append((q, (q+a/2)%a))
+        for i in range(1, a/2):
+            result.append(((q - i + a) % a, (q + i) % a))
+        return result
+
+
 
 
 class Pairer(object):
 
     def __init__(self, n):
-        '''
-        '''
+        """
+        INPUT: n, an even natural number
+        Constructs a Pairer object for a given even number of students, n.
+        """
         self.n = n
 
-        if (n % 2 == 1 or n == 0 or n < 0):
-            raise ValueError("N={0} IS BAD I CANT EVEN".format(n))
+        if n % 2 == 1 or n <= 0:
+            raise ValueError("N={0} IS UNACCEPTABLE I CANT EVEN".format(n))
         a = n
         b = 1
-        while (a % 4 == 0):
+        while a % 4 == 0:
             a /= 2
             b *= 2
+        # a is an even number not divisible by 4
         self.a = a
+        # b is a power of 2
         self.b = b
 
+    def get_pairing(self, day):
+        r, s = f(day, self.b)
+        p1 = get_pairing_power_of_2(s, self.b)
+        print p1
+        p2 = get_pairing_double_odd(r, self.a)
+        print p2
+        return combine_pairings(p1, p2, self.a)
 
-    def f(self, k):
-        """
-        INPUT: integer between 0 and n
-        Canonical translation from k -> (r,s)
-        Finds (r, s) such that s < self.a and r * self.b + s = k
-        """
-        return (k/self.b, k%self.b)
-
-    def finv(self, t):
-        """
-        INPUT: (r,s) as above
-        OUTPUT: k as above
-        Canonical translation (r,s) -> k
-        """
-        r, s = t
-        return r * self.b + s
-
-
-    # TODO 
-    # Jack's model to find what to do on day r with a students
-    # Brian's model to find what to do on day s with b students
-    # Combine using f and finv function to combine
+n=12
+p = Pairer(n)
+for i in range(1, n):
+    print p.get_pairing(i)
+    print ''
--- code/test_pairer.py
+++ code/test_pairer.py
@@ -1,6 +1,6 @@
-
-from pairer import Pairer
+from pairer import *
 import nose.tools as n
+
 
 def test_init():
     p = Pairer(20)
@@ -12,9 +12,18 @@ def test_init():
     n.assert_equal(q.a, 6)
     n.assert_equal(q.b, 4)
 
+
 def test_f():
-    for i in range(1, 200):
-        print i
-        p = Pairer(i * 2)
-        for j in range(1, i * 2):
-            n.assert_equal(j, p.finv(p.f(j)))
+    for b in range(1, 200):
+        for k in range(1, 200):
+            n.assert_equal(k, finv(f(k, b)[0], f(k, b)[1], b))
+
+
+def test_get_pairing_power_of_2():
+    n.assert_equal(get_pairing_power_of_2(1,4), [(0, 1), (1, 0), (2, 3), (3, 2)])
+
+
+def test_combine_pairings():
+    p1 = get_pairing_power_of_2(3, 4)
+    p2 = get_pairing_power_of_2(1, 4)
+    n.assert_equal(combine_pairings(p1, p2, 4), [(0, 13), (1, 12), (2, 15), (3, 14), (4, 9), (5, 8), (6, 11), (7, 10), (8, 5), (9, 4), (10, 7), (11, 6), (12, 1), (13, 0), (14, 3), (15, 2)])
